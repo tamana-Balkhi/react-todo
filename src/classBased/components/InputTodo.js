fda5b8eeba310d4ac6d56c7ea3ab24ecee2e69c0
tamana-Balkhi <tamana.balkhi1377@gmail.com>
2022-09-14 01:30:52 +0430

    Created class based components

---
+++ src/classBased/components/InputTodo.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react"
+
+class InputTodo extends Component {
+  state = {
+    fName: "",
+    lastName: "",
+  }
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    })
+  }
+  handleSubmit = e => {
+    e.preventDefault()
+    if (this.state.title.trim()) {
+      this.props.addTodoProps(this.state.title)
+      this.setState({
+        title: "",
+      })
+    } else {
+      alert("Please write item")
+    }
+  }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} className="form-container">
+        <input type="text" placeholder="Add todo..." value={this.state.title} name="title" onChange={this.onChange} />
+        <button className="input-submit">Submit</button>
+      </form>
+    )
+  }
+}
+export default InputTodo
---
+++ src/classBased/components/TodoItem.js
@@ -0,0 +1,58 @@
+import React from "react"
+import styles from "./TodoItem.module.css"
+
+class TodoItem extends React.Component {
+  handleEditing = () => {
+    this.setState({
+      editing: true,
+    })
+  }
+  state = {
+    editing: false,
+  }
+  handleUpdatedDone = event => {
+    if (event.key === "Enter") {
+      this.setState({ editing: false })
+    }
+  }
+  componentWillUnmount() {
+    console.log("Cleaning up...")
+  }
+  render() {
+    const completedStyle = {
+      fontStyle: "italic",
+      color: "#595959",
+      opacity: 0.4,
+      textDecoration: "line-through",
+    }
+    let viewMode = {}
+    let editMode = {}
+
+    if (this.state.editing) {
+      viewMode.display = "none"
+    } else {
+      editMode.display = "none"
+    }
+    const { completed, id, title } = this.props.todo
+    return <li className={styles.item}>
+      <div onDoubleClick={this.handleEditing} style={viewMode}>...</div>
+      <input type="text" style={editMode} className={styles.textInput} value={title}
+        onChange={e => {
+          this.props.setUpdate(e.target.value, id)
+        }} onKeyDown={this.handleUpdatedDone} />
+      <input
+        type="checkbox"
+        checked={completed}
+        onChange={() => this.props.handleChangeProps(id)}
+      />
+      <button onClick={() => this.props.deleteTodoProps(id)}>
+        Delete
+</button>
+      <span style={this.props.todo.completed ? completedStyle : null}>
+        {title}
+      </span>
+    </li>
+  }
+}
+
+export default TodoItem
